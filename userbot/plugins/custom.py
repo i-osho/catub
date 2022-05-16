b08477d887577238208d53bee9b4888399f35b26
Osho Raj <65038390+i-osho@users.noreply.github.com>
2022-05-16 17:52:09 +0530

    Update custom.py

--- userbot/plugins/custom.py
+++ userbot/plugins/custom.py
@@ -1,59 +1,54 @@
-from PIL import Image
-from telegraph import Telegraph, exceptions, upload_file
-from telethon.tl import types
+from telegraph import upload_file
+from telethon.tl.functions.users import GetFullUserRequest
+from urlextract import URLExtract
+from validators.url import url
 
-from userbot import catub
+from userbot import BOTLOG_CHATID, catub
 from userbot.core.logger import logging
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
-from . import BOTLOG_CHATID
 
-plugin_category = "utils"
+plugin_category = "tools"
 LOGS = logging.getLogger(__name__)
 cmdhd = Config.COMMAND_HAND_LER
 
-telegraph = Telegraph()
-r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
-auth_url = r["auth_url"]
-
-
-def resize_image(image):
-    im = Image.open(image)
-    im.save(image, "PNG")
-
-
+extractor = URLExtract()
 vlist = [
-    "ALIVE_CHANNEL",
-    "LOG_LIVE",
     "ALIVE_PIC",
     "ALIVE_EMOJI",
-    "ALIVE_TEXT",
     "ALIVE_TEMPLATE",
+    "ALIVE_TEXT",
     "ALLOW_NSFW",
+    "CHANGE_TIME",
+    "DEFAULT_BIO",
+    "DEFAULT_NAME",
+    "DEFAULT_PIC",
+    "DEFAULT_USER",
+    "DIGITAL_PIC",
+    "FIRST_NAME",
     "HELP_EMOJI",
-    "HELP_PIC",
     "HELP_TEXT",
     "IALIVE_PIC",
-    "INLINE_PIC",
-    "PING_PICS",
+    "LAST_NAME",
+    "PING_PIC",
     "PING_TEMPLATE",
     "PM_PIC",
     "PM_TEXT",
     "PM_BLOCK",
     "MAX_FLOOD_IN_PMS",
-    "S_ID",
-    "S_S",
     "START_TEXT",
-    "START_PIC",
+    "S_S",
+    "S_ID",
+    "BOT_START_PIC",
     "NO_OF_ROWS_IN_HELP",
     "NO_OF_COLUMNS_IN_HELP",
     "CUSTOM_STICKER_PACKNAME",
-    "PUBLIC_CHANNEL_ID",
 ]
 
 oldvars = {
+    "PM_PIC": "pmpermit_pic",
     "PM_TEXT": "pmpermit_txt",
     "PM_BLOCK": "pmblock",
 }
@@ -70,7 +65,7 @@ oldvars = {
             "get": "To show the already existing var value.",
             "del": "To delete the existing value",
         },
-        "var name": "**[List of Database Vars]**(https://catuserbot.gitbook.io/catuserbot/data-vars-setup)",
+        "var name": "**[list of vars]**(https://catuserbot.gitbook.io/catuserbot/data-vars-setup)",
         "usage": [
             "{tr}setdv <var name> <var value>",
             "{tr}getdv <var name>",
@@ -98,66 +93,77 @@ async def bad(event):  # sourcery no-metrics
         vname, vinfo = vname.split(" ", 1)
     reply = await event.get_reply_message()
     if not vinfo and reply:
-        # ==============================================================================
-        # These useless auto link gen for pic/sticker/everything by https://t.me/i_osho
-        try:
-            animated = reply.document.mime_type == "application/x-tgsticker"
-        except:
-            animated = None
-        try:
-            size = reply.file.size / 1024
-            if (size >= 5000) or animated:
-                await event.edit("`Making message link...`")
-                if reply.chat.username and type(reply.peer_id) != types.PeerUser:
-                    username = reply.chat.username
-                    msg_id = reply.id
-                    vinfo = f"https://t.me/{username}/{msg_id}"
-                else:
-                    channel_id = gvarstatus("PUBLIC_CHANNEL_ID")
-                    if channel_id == None:
-                        return await edit_delete(
-                            event,
-                            "Add `PUBLIC_CHANNEL_ID` in dv for auto link gen to work`",
-                        )
-                    chat = await event.client.get_entity(int(channel_id))
-                    sent = await event.client.send_file(chat.id, reply.media)
-                    vinfo = f"https://t.me/{chat.username}/{sent.id}"
-            elif (type(reply.media) == types.MessageMediaDocument) or (
-                type(reply.media) == types.MessageMediaPhoto
-            ):
-                await event.edit("`Creating link...`")
-                downloaded_file_name = await event.client.download_media(
-                    reply, Config.TEMP_DIR
-                )
-                try:
-                    if downloaded_file_name.endswith((".webp")):
-                        resize_image(downloaded_file_name)
-                    media_urls = upload_file(downloaded_file_name)
-                    vinfo = f"https://telegra.ph{media_urls[0]}"
-                except AttributeError:
-                    return await event.edit("`Error while making link`")
-                except exceptions.TelegraphException as exc:
-                    return await event.edit(f"**Error** : `{str(exc)}`")
-        except AttributeError:
-            vinfo = reply.text
-            # ==============================================================================
+        vinfo = reply.text
     if vname in vlist:
         if vname in oldvars:
             vname = oldvars[vname]
         if cmd == "set":
-            if not vinfo and vname == "ALIVE_TEMPLATE":
-                return await edit_delete(
-                    event, "**💠 Check @cat_alive for alive teplate types.**", 60
-                )
-            if not vinfo:
-                return await edit_delete(
-                    event, f"Give some values which you want to save for **{vname}**"
-                )
-            vinfo.split(" ")
-            # for i in check:
-            #    if "PIC" in vname and not url(i):
-            #        return await edit_delete(event, "**Give me a correct link...**")
-            addgvar(vname, vinfo)
+            if vname == "DEFAULT_USER":
+                if not vinfo or vinfo != "Me":
+                    return await edit_delete(
+                        event,
+                        "**To save your Current Profile info Set the value:**\\n `.setdv DEFAULT_USER Me`",
+                    )
+
+                USERINFO = await catub.get_entity(catub.uid)
+                FULL_USERINFO = (await catub(GetFullUserRequest(catub.uid))).full_user
+                addgvar("FIRST_NAME", USERINFO.first_name)
+                addgvar("DEFAULT_NAME", USERINFO.first_name)
+                if USERINFO.last_name:
+                    addgvar(
+                        "DEFAULT_NAME",
+                        f"{USERINFO.first_name}  {USERINFO.first_name}",
+                    )
+                    addgvar("LAST_NAME", USERINFO.last_name)
+                elif gvarstatus("LAST_NAME"):
+                    delgvar("LAST_NAME")
+                if FULL_USERINFO.about:
+                    addgvar("DEFAULT_BIO", FULL_USERINFO.about)
+                elif gvarstatus("DEFAULT_BIO"):
+                    delgvar("DEFAULT_BIO")
+                try:
+                    photos = await catub.get_profile_photos(catub.uid)
+                    myphoto = await catub.download_media(photos[0])
+                    myphoto_urls = upload_file(myphoto)
+                    addgvar("DEFAULT_PIC", f"https://telegra.ph{myphoto_urls[0]}")
+                except IndexError:
+                    if gvarstatus("DEFAULT_PIC"):
+                        delgvar("DEFAULT_PIC")
+                usrln = gvarstatus("LAST_NAME") or None
+                usrbio = gvarstatus("DEFAULT_BIO") or None
+                usrphoto = gvarstatus("DEFAULT_PIC") or None
+                vinfo = f'**Name:** `{gvarstatus("DEFAULT_NAME")}`\n**First Name:** `{gvarstatus("FIRST_NAME")}`\n**Last Name:** `{usrln}`\n**Bio:** `{usrbio}`\n**Photo:** `{usrphoto}`'
+            else:
+                if not vinfo and vname in ["ALIVE_TEMPLATE", "PING_TEMPLATE"]:
+                    return await edit_delete(event, "Check @cat_alive")
+                if not vinfo:
+                    return await edit_delete(
+                        event,
+                        f"Give some values which you want to save for **{vname}**",
+                    )
+                check = vinfo.split(" ")
+                for i in check:
+                    if vname == "DEFAULT_PIC" and not url(i):
+                        return await edit_delete(event, "**Give me a correct link...**")
+                    elif vname == "DIGITAL_PIC" and not url(i):
+                        return await edit_delete(event, "**Give me a correct link...**")
+                    elif (("PIC" in vname) or ("pic" in vname)) and not url(i):
+                        return await edit_delete(event, "**Give me a correct link...**")
+                    elif (
+                        vname == "DIGITAL_PIC"
+                        or vname == "DEFAULT_PIC"
+                        or vname == "BOT_START_PIC"
+                    ) and url(i):
+                        vinfo = i
+                        break
+                    elif not "PIC" in vname:
+                        break
+                if vname == "DEFAULT_BIO" and len(vinfo) > 70:
+                    return await edit_or_reply(
+                        event,
+                        f"No of characters in your bio must not exceed 70 so compress it and set again\n`{vinfo}`",
+                    )
+                addgvar(vname, vinfo)
             if BOTLOG_CHATID:
                 await event.client.send_message(
                     BOTLOG_CHATID,
@@ -171,9 +177,18 @@ async def bad(event):  # sourcery no-metrics
         if cmd == "get":
             var_data = gvarstatus(vname)
             await edit_delete(
-                event, f"📑 Value of **{vname}** is  `{var_data}`", time=20
+                event, f"📑 Value of **{vname}** is  ```{var_data}```", time=20
             )
         elif cmd == "del":
+            if vname == "DEFAULT_USER":
+                delgvar("FIRST_NAME")
+                delgvar("DEFAULT_NAME")
+                if gvarstatus("LAST_NAME"):
+                    delgvar("LAST_NAME")
+                if gvarstatus("DEFAULT_BIO"):
+                    delgvar("DEFAULT_BIO")
+                if gvarstatus("DEFAULT_PIC"):
+                    delgvar("DEFAULT_PIC")
             delgvar(vname)
             if BOTLOG_CHATID:
                 await event.client.send_message(
@@ -193,7 +208,7 @@ async def bad(event):  # sourcery no-metrics
 
 
 @catub.cat_cmd(
-    pattern="custom (pmpermit|pmblock|startmsg)$",
+    pattern="custom (pmpermit|pmpic|pmblock|startmsg)$",
     command=("custom", plugin_category),
     info={
         "header": "To customize your CatUserbot.",
@@ -201,6 +216,7 @@ async def bad(event):  # sourcery no-metrics
             "pmpermit": "To customize pmpermit text. ",
             "pmblock": "To customize pmpermit block message.",
             "startmsg": "To customize startmsg of bot when some one started it.",
+            "pmpic": "To customize pmpermit pic. Reply to media url or text containing media.",
         },
         "custom": {
             "{mention}": "mention user",
@@ -218,7 +234,9 @@ async def bad(event):  # sourcery no-metrics
             "{warns}": "warns",
             "{remwarns}": "remaining warns",
         },
-        "usage": "{tr}custom <option> reply",
+        "usage": [
+            "{tr}custom <option> reply",
+        ],
         "NOTE": "You can set,fetch or delete these by `{tr}setdv` , `{tr}getdv` & `{tr}deldv` as well.",
     },
 )
@@ -237,6 +255,12 @@ async def custom_catuserbot(event):
         addgvar("pmblock", text)
     if input_str == "startmsg":
         addgvar("START_TEXT", text)
+    if input_str == "pmpic":
+        urls = extractor.find_urls(reply.text)
+        if not urls:
+            return await edit_delete(event, "`the given link is not supported`", 5)
+        text = " ".join(urls)
+        addgvar("pmpermit_pic", text)
     await edit_or_reply(event, f"__Your custom {input_str} has been updated__")
     if BOTLOG_CHATID:
         await event.client.send_message(
@@ -245,3 +269,52 @@ async def custom_catuserbot(event):
                     \n**{input_str}** is updated newly in database as below",
         )
         await event.client.send_message(BOTLOG_CHATID, text, silent=True)
+
+
+@catub.cat_cmd(
+    pattern="delcustom (pmpermit|pmpic|pmblock|startmsg)$",
+    command=("delcustom", plugin_category),
+    info={
+        "header": "To delete costomization of your CatUserbot.",
+        "options": {
+            "pmpermit": "To delete custom pmpermit text",
+            "pmblock": "To delete custom pmpermit block message",
+            "pmpic": "To delete custom pmpermit pic.",
+            "startmsg": "To delete custom start message of bot when some one started it.",
+        },
+        "usage": [
+            "{tr}delcustom <option>",
+        ],
+        "NOTE": "You can set,fetch or delete these by `{tr}setdv` , `{tr}getdv` & `{tr}deldv` as well.",
+    },
+)
+async def custom_catuserbot(event):
+    "To delete costomization of your CatUserbot."
+    input_str = event.pattern_match.group(1)
+    if input_str == "pmpermit":
+        if gvarstatus("pmpermit_txt") is None:
+            return await edit_delete(event, "__You haven't customzied your pmpermit.__")
+        delgvar("pmpermit_txt")
+    if input_str == "pmblock":
+        if gvarstatus("pmblock") is None:
+            return await edit_delete(event, "__You haven't customzied your pmblock.__")
+        delgvar("pmblock")
+    if input_str == "pmpic":
+        if gvarstatus("pmpermit_pic") is None:
+            return await edit_delete(event, "__You haven't customzied your pmpic.__")
+        delgvar("pmpermit_pic")
+    if input_str == "startmsg":
+        if gvarstatus("START_TEXT") is None:
+            return await edit_delete(
+                event, "__You haven't customzied your start msg in bot.__"
+            )
+        delgvar("START_TEXT")
+    await edit_or_reply(
+        event, f"__successfully deleted your customization of {input_str}.__"
+    )
+    if BOTLOG_CHATID:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"#DEL_DATAVAR\
+                    \n**{input_str}** is deleted from database",
+        )
